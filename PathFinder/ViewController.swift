ecfa9c622f42b4244df9d43f3469fb6f7facfde2
Jack Michaletz <john.michaletz@cuw.edu>
2020-03-14 13:03:30 -0500

    Initial Commit

---
+++ PathFinder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PathFinder
+//
+//  Created by Jack Michaletz on 3/14/20.
+//  Copyright © 2020 Jack Michaletz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
